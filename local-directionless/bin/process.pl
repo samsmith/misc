4dcae32ac5f91607981fb7a9d1b99b0d411f46c0
Sam <gihub.com@msmith.net>
2012-02-17 12:30:55 +0000

    current version

--- local-directionless/bin/process.pl
+++ local-directionless/bin/process.pl
@@ -7,13 +7,15 @@ use Digest::MD5 qw/md5_hex/;
 my $csv_file= shift;
 my $dir= shift || "usage: $0 file.csv pages/\n";
 my %filemapping;
+#use Text::Similarity::Overlaps;
+#my $overlaps= Text::Similarity::Overlaps->new({'normalize' => 1});
 my $authority_info;
 my %pageinfo;
 my %broken;
 
 {
 	&setup();
-	#&obviously_broken();
+	&obviously_broken();
 	&slightly_harder_to_find();
 	&output();
 }
@@ -33,7 +35,12 @@ sub setup {
 	while ( my $row = $csv->getline( $fh ) ) {
 		# hash of md5 = url
 		next if $row->[1] eq 'SNAC';
-		#warn $row->[6];
+
+		if (defined $filemapping{md5_hex($row->[6]).".html"} and $filemapping{md5_hex($row->[6]).".html"}->{'authority'} ne $row->[0]) {
+			&report_broken(md5_hex($row->[6]).".html", "cross LA link, also for $row->[0]");
+			next;
+		}
+#warn $row->[6];
 		$filemapping{md5_hex($row->[6]).".html"}->{'url'}= $row->[6];
 		$filemapping{md5_hex($row->[6]).".html"}->{'authority'}= $row->[0];
 		$filemapping{md5_hex($row->[6]).".html"}->{'service'}= $row->[3];
@@ -50,23 +57,31 @@ sub setup {
 
 
 sub obviously_broken {
+	my %seen;
 
 	# for each file in the directory, which are zero length files?
 	# that can not possibly be valid
 	opendir (PAGES, "$dir") || die "can't opendir $dir: $!";
 	foreach my $file (readdir(PAGES)) {
-		if (-z $file) {&report_broken($file, "zero length"); next} 
-		if (-s $file < 2048 ) { &report_broken($file, "very small file: redirect?"); next }  # all files tested were broken
+		if (-z "$dir/$file") {&report_broken($file, "zero length"); next} 
+		if (-s "$dir/$file" < 2048 ) { &report_broken($file, "very small file: redirect?"); next }  # all files tested were broken
 		if (`grep 'cannot be found' pages/$file`) { &report_broken($file, "matches 'cannot be found'"); next } # bodge, but effective
 		if (`grep 'error 404' pages/$file`) { &report_broken($file, "error 404"); next } # bodge, but effective
 		if (`grep 'not found' pages/$file`) { &report_broken($file, "matches 'not found'"); next } # bodge, but effective
 		if (`grep ' 404[< ] ' pages/$file`) { &report_broken($file, "404"); next } # bodge, but effective
+		
+		my $this_hash= `md5 -r pages/$file`;
+		if (defined $seen{$this_hash}) {
+			&report_broken($file, "exact duplicate of $seen{$this_hash}", "multiple ok");
+			$seen{$this_hash}.= ", $filemapping{$file}->{'url'}";
+		} else {
+			$seen{$this_hash}= $filemapping{$file}->{'url'};
+		}
 		# duplicate URLs don't get counted below, but are arguably broken
 		# it's also possible to crowdsource this as "does page X in the iframe below contain the informatin that should be in it according to our index"
 
 		# additional tests here are left as an exercise for the reader or @dafyddbach
 	}
-
 
 }
 
@@ -75,37 +90,38 @@ sub slightly_harder_to_find {
 	my %done_compare;
 
 	foreach my $authority (keys %$authority_info) {
+		my $now= scalar localtime;
+		warn $now . " " . $authority;
+		next if $authority eq ''; #warn $authority;
 		my @sortish= sort values %{$authority_info->{$authority}};  # gets us services sorted by memory id  -- effectively randomly
-
 		foreach my $service (sort keys %{$authority_info->{$authority}}) {
-			foreach my $compare (@sortish) { #[0..2]) { # all files might work better here rather than just the first 2
+			#foreach my $compare (@sortish) { #[0..2]) { # all files might work better here rather than just the first 2
+			foreach my $compare (@sortish[0..2]) { # all files might work better here rather than just the first 2
 				next if not defined $compare; # <=2 files.
 				next if $broken{$authority_info->{$authority}->{$service}->{'hash'}}; #already broken
 				next if $broken{$compare->{'hash'}}; # broken compare file
 				my $file1= 'pages/' . $authority_info->{$authority}->{$service}->{'hash'};
 				my $file2= 'pages/' . $compare->{'hash'};
 				next if ($file1 eq $file2); #don't compare with self
-				next unless -e $file1;
-				next unless -e $file2;
-				next if -z $file1;
-				next if -z $file2;
-
+				next unless -e $file1; next unless -e $file2; next if -z $file1; next if -z $file2; 
 				next if defined $done_compare{'matched'}->{"$service$compare"}; # no double counting duplicate URLs for different services
 				$done_compare{'matched'}->{"$service$compare"}++;
-
+#warn "hi";
 				#warn "$file1 $file2";
-
 				$done_compare{$authority}->{$service}->{'comparisons'}++;
-				my $output = `diff -C 0 $file1 $file2`;
+				my $output='';
+
+				$output = `diff -C 0 $file1 $file2`;
 				my $lines_different=0;
 
 				foreach my $line (split /\n/, $output){ 
 					$lines_different+= 0.5 if $line =~ m#^!#; # lines that change all start with !, and any different line is there twice.
 				}
 
-				if ($lines_different < 10 ) {
+				if ($lines_different < 15 ) {
 					$file1=~ s#^pages/##;
-					&report_broken($file1, "seems similar to $file2", "multiple reports ok");
+					$file2=~ s#^pages/##;
+					&report_broken($file1, "seems similar to $filemapping{$file2}->{'url'}", "multiple reports ok");
 					next;
 				}
 			}
@@ -122,8 +138,12 @@ sub slightly_harder_to_find {
 
 sub report_broken {
 	my ($filename, $why, $multiple_reports_ok)= @_;
-	next if (defined $broken{$filename} and not defined $multiple_reports_ok); # already broken for another reason
-	$broken{$filename}.= "$why\n";
+	return if (defined $broken{$filename} and not defined $multiple_reports_ok); # already broken for another reason
+	if (defined $broken{$filename}) {
+		$broken{$filename}.= ", $why";
+	} else {
+		$broken{$filename}.= "$why";
+	}
 }
 
 
